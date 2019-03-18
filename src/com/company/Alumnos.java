254c45ba65774a5e4b0b2457b41d608eb3c505de
Julio Luna <jstuartluna@gmail.com>
2019-03-18 01:19:18 -0600

    Alumnos

--- src/com/company/Alumnos.java
+++ src/com/company/Alumnos.java
@@ -2,8 +2,11 @@ package com.company;
 
 public class Alumnos extends Personas {
     protected String carreraestudia;
+    protected String numerodecuentaalumno;
 
-    public Alumnos(String nombre, String apellido, String numerodecuenta, String identidad, String edad) {
+    public Alumnos(String nombre, String apellido, String numerodecuenta, String identidad, String edad, String carreraestudia, String numerodecuentaalumno) {
         super(nombre, apellido, numerodecuenta, identidad, edad);
+        this.carreraestudia = carreraestudia;
+        this.numerodecuentaalumno = numerodecuentaalumno;
     }
 }
